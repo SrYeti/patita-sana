b894810a6e1bfc6ba35f3827edd6729abcd78e72
Camilo <119712665+SrYeti@users.noreply.github.com>
2025-05-14 18:48:47 -0400

    Update register.page.ts

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -20,27 +20,3 @@ export class RegisterPage {
     console.log('Registro:', this.nombre, this.email, this.password, this.confirmPassword);
   }
 }
-
-
-/*
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-
-@Component({
-  selector: 'app-register',
-  templateUrl: './register.page.html',
-  styleUrls: ['./register.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
-})
-export class RegisterPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
-*/
