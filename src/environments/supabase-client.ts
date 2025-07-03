aedf02509d322a8331149538469d398190e205ee
SrYeti <k.milo.arana@gmail.com>
2025-07-03 02:11:47 -0400

    🤪

---
+++ src/environments/supabase-client.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://tlyqsjuthziajcmwkzek.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRseXFzanV0aHppYWpjbXdremVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2MjAwNTcsImV4cCI6MjA2MzE5NjA1N30.VXMnma0a6HhEwU16r1BTMF_18676UReBjT7jZNjUVeo';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
